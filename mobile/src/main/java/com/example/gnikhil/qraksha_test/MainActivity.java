0e157ab8d38b7418d8c7c47588adb873abc526cf
nikhilendrag <nikhilendra.g@gmail.com>
2018-07-29 15:34:31 +0530

    New UI for Mobile, SOS button at the top, followed by My Information and Emergency Information sections.Data entered in My Information and Emergency Information will be saved to the disk and will be reloaded on App restart.
    Used Toast to display short notes on UI instead of updating TextView.
    
    Play Police siren when Panic sequence is started (either triggered manually on Mobile App or remotely from Wear App)

--- mobile/src/main/java/com/example/gnikhil/qraksha_test/MainActivity.java
+++ mobile/src/main/java/com/example/gnikhil/qraksha_test/MainActivity.java
@@ -1,18 +1,21 @@
 package com.example.gnikhil.qraksha_test;
 
+import android.content.SharedPreferences;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.content.BroadcastReceiver;
 import android.util.Log;
-import android.widget.Button;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.support.v4.content.LocalBroadcastManager;
-import android.os.Handler;
-import android.os.Message;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.wearable.Node;
 import com.google.android.gms.tasks.Task;
@@ -25,43 +28,109 @@ import java.util.concurrent.ExecutionException;
 
 public class MainActivity extends AppCompatActivity  {
 
-    Button talkbutton;
-    TextView textview;
-    protected Handler myHandler;
+    ImageButton panicbutton;
+    Button saveMyInfoButton;
+    Button saveEmergencyInfoButton;
+    TextView myInfoName,myInfoPhone, myInfoEmail, emergencyInfoName, emergencyInfoPhone, emergencyInfoEmail;
+
     private static final String PANIC_STARTED_PATH = "/qraksha_started";
     private static final String TAG = "QRaksha";
+    private SharedPreferences sharedpreferences;
+    public static final String mypreference = "mypref";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        talkbutton = findViewById(R.id.talkButton);
-        textview = findViewById(R.id.textView);
+        myInfoName = findViewById(R.id.name_field);
+        myInfoPhone = findViewById(R.id.phone_field);
+        myInfoEmail = findViewById(R.id.email_field);
 
-        //Create a message handler//
+        emergencyInfoName = findViewById(R.id.emergency_name_field);
+        emergencyInfoPhone = findViewById(R.id.emergency_phone_field);
+        emergencyInfoEmail = findViewById(R.id.emergency_email_field);
 
-        myHandler = new Handler(new Handler.Callback() {
+        panicbutton = findViewById(R.id.panicButton);
+        panicbutton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public boolean handleMessage(Message msg) {
-                Bundle stuff = msg.getData();
-                messageText(stuff.getString("messageText"));
-                return true;
+            public void onClick(View v) {
+                panicStart();
+            }
+        });
+
+        saveEmergencyInfoButton = findViewById(R.id.save_emergency_info_id);
+        saveEmergencyInfoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                putEmergencyInfo(v);
+            }
+        });
+
+        saveMyInfoButton = findViewById(R.id.save_my_info_id);
+        saveMyInfoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                putMyInfo(v);
             }
         });
 
         //Register to receive local broadcasts, which we'll be creating in the next step//
-
         IntentFilter messageFilter = new IntentFilter(Intent.ACTION_SEND);
         Receiver messageReceiver = new Receiver();
         LocalBroadcastManager.getInstance(this).registerReceiver(messageReceiver, messageFilter);
 
+        sharedpreferences = getSharedPreferences(mypreference,Context.MODE_PRIVATE); // 0 - for private mode
+        getMyInfo();
+        getEmergencyInfo();
     }
 
-    public void messageText(String newinfo) {
-        if (newinfo.compareTo("") != 0) {
-            textview.append("\n" + newinfo);
+    public void putMyInfo(View v){
+        SharedPreferences.Editor editor = sharedpreferences .edit();
+        editor.putString("my_info_name", myInfoName.getText().toString());
+        editor.putString("my_info_phone", myInfoPhone.getText().toString());
+        editor.putString("my_info_email", myInfoEmail.getText().toString());
+
+        editor.apply(); // commit changes
+        Log.e(TAG, "Committed my_info");
+    }
+
+    public void getMyInfo(){
+
+        if(sharedpreferences.contains("my_info_name")){
+            myInfoName.setText(sharedpreferences .getString("my_info_name", ""));
         }
+        if(sharedpreferences.contains("my_info_phone")){
+            myInfoPhone.setText(sharedpreferences .getString("my_info_phone", ""));
+        }
+        if(sharedpreferences.contains("my_info_email")){
+            myInfoEmail.setText(sharedpreferences .getString("my_info_email", ""));
+        }
+        Log.e(TAG, "Extracted my_info");
+    }
+
+    public void putEmergencyInfo(View v){
+        SharedPreferences.Editor editor = sharedpreferences .edit();
+        editor.putString("emergency_info_name", emergencyInfoName.getText().toString());
+        editor.putString("emergency_info_phone", emergencyInfoPhone.getText().toString());
+        editor.putString("emergency_info_email", emergencyInfoEmail.getText().toString());
+
+        editor.apply(); // commit changes
+        Log.e(TAG, "Committed emergency_info");
+    }
+
+    public void getEmergencyInfo(){
+
+        if(sharedpreferences.contains("emergency_info_name")){
+            emergencyInfoName.setText(sharedpreferences .getString("emergency_info_name", ""));
+        }
+        if(sharedpreferences.contains("emergency_info_phone")){
+            emergencyInfoPhone.setText(sharedpreferences .getString("emergency_info_phone", ""));
+        }
+        if(sharedpreferences.contains("emergency_info_email")){
+            emergencyInfoEmail.setText(sharedpreferences .getString("emergency_info_email", ""));
+        }
+        Log.e(TAG, "Extracted emergency_info");
     }
 
     //Define a nested class that extends BroadcastReceiver//
@@ -71,21 +140,33 @@ public class MainActivity extends AppCompatActivity  {
 
             //Upon receiving each message from the wearable, display the following text//
             String message = "onReceive : SOS on wearable";
-            Long tsLong = System.currentTimeMillis()/1000;
-            String ts = tsLong.toString();
-            textview.setText(ts + " : " + message);
+            int duration = Toast.LENGTH_SHORT;
+
+            Toast toast = Toast.makeText(context, message, duration);
+            toast.show();
+
+            panicStart();
+
             new NewThread(PANIC_STARTED_PATH, message).start();
             Log.e(TAG, message);
         }
     }
 
-    public void talkClick(View v) {
-        String message = " : talkClick : PANIC sequence started";
-        Long tsLong = System.currentTimeMillis()/1000;
-        String ts = tsLong.toString();
-        textview.setText(ts + " : " + message);
+    public void panicStart() {
+        String message = "panicStart : PANIC sequence started";
+        int duration = Toast.LENGTH_SHORT;
 
-		//Sending a message can block the main UI thread, so use a new thread//
+        Toast toast = Toast.makeText(getApplicationContext(), message, duration);
+        toast.show();
+
+        // TODO : Comment below for testing, Uncomment while submitting
+        //AudioManager audioManager = (AudioManager)getSystemService(Context.AUDIO_SERVICE);
+        //audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC), 0);
+
+        MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.police_siren);
+        mediaPlayer.start();
+
+        //Sending a message can block the main UI thread, so use a new thread//
         new NewThread(PANIC_STARTED_PATH, message).start();
         Log.e(TAG, message);
     }
@@ -116,9 +197,6 @@ public class MainActivity extends AppCompatActivity  {
                         Integer result = Tasks.await(sendMessageTask);
                         String message = "Message sent successfully to wearable";
                         Log.e(TAG, message);
-                        Long tsLong = System.currentTimeMillis()/1000;
-                        String ts = tsLong.toString();
-                        textview.setText(ts+ " : " + message);
 
                         //if the Task fails, then…..//
                     } catch (ExecutionException exception) {
